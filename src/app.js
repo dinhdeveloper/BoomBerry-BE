268d46af8747d032450a0fa9b965dc5cb9f35796
DinhTC <tc.dinh@shinhanfinance.com.vn>
2025-12-25 16:54:48 +0700

    change static

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
+
+// ===== Serve static files =====
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 // Routes
 app.use("/api/categories", require("./routes/categories.route"));
