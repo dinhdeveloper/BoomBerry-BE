5e57566a4def3f0716ecd367cf4d76e462e81095
DinhTC <tc.dinh@shinhanfinance.com.vn>
2025-12-25 16:59:07 +0700

    change folder static load image

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-// ===== Serve static files =====
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+// Serve static folder
+// Nếu assets nằm trong public, URL sẽ bắt đầu từ /assets
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // Routes
 app.use("/api/categories", require("./routes/categories.route"));
